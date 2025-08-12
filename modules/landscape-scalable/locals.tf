ebaec3d6f23e4591c4d3c954c15a5b26535cc3bf
jansdhillon <jan.dhillon@canonical.com>
2025-08-12 06:54:34 -0600

    fix null revision check

--- modules/landscape-scalable/locals.tf
+++ modules/landscape-scalable/locals.tf
@@ -4,6 +4,6 @@ locals {
   self_signed = lookup(var.haproxy.config, "ssl_key", null) == null || lookup(var.haproxy.config, "ssl_cert", null) == null
   root_url    = "${var.hostname}.${var.domain}"
   using_smtp  = !local.self_signed && lookup(var.landscape_server.config, "smtp_password", null) != null && lookup(var.landscape_server.config, "smtp_host", null) != null && lookup(var.landscape_server.config, "smtp_username", null) != null
-  legacy_amqp = var.landscape_server.channel != "latest-stable/edge" || var.landscape_server.revision <= 141
+  legacy_amqp = var.landscape_server.channel != "latest-stable/edge" || (var.landscape_server.revision != null && var.landscape_server.revision <= 141)
   model       = "landscape"
 }
