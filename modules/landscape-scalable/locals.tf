81eff37cfc874a4fc2d6f30894c661126278de5f
jansdhillon <jan.dhillon@canonical.com>
2025-08-12 07:01:45 -0600

    fix hardcoded model name

--- modules/landscape-scalable/locals.tf
+++ modules/landscape-scalable/locals.tf
@@ -5,5 +5,5 @@ locals {
   root_url    = "${var.hostname}.${var.domain}"
   using_smtp  = !local.self_signed && lookup(var.landscape_server.config, "smtp_password", null) != null && lookup(var.landscape_server.config, "smtp_host", null) != null && lookup(var.landscape_server.config, "smtp_username", null) != null
   legacy_amqp = var.landscape_server.channel != "latest-stable/edge" || (var.landscape_server.revision != null && var.landscape_server.revision <= 141)
-  model       = "landscape"
+  model       = var.model
 }
