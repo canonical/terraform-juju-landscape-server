7bb4e65061dcd8e7a67cf1c4b4f69d5b3da39772
jansdhillon <jan.dhillon@canonical.com>
2025-08-15 13:01:47 -0600

    fix: use `var.rabbitmq_server.app_name` instead of hardcoding it

--- modules/landscape-scalable/locals.tf
+++ modules/landscape-scalable/locals.tf
@@ -1,6 +1,10 @@
 # © 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 locals {
-  self_signed = lookup(var.haproxy.config, "ssl_key", null) == null || lookup(var.haproxy.config, "ssl_cert", null) == null
+  self_signed = (
+    lookup(var.haproxy.config, "ssl_key", null) == null ||
+    lookup(var.haproxy.config, "ssl_cert", null) == null ||
+    lookup(var.haproxy.config, "ssl_cert", null) == "SELFSIGNED"
+  )
   legacy_amqp = var.landscape_server.channel != "latest-stable/edge" || (var.landscape_server.revision != null && var.landscape_server.revision <= 141)
 }
